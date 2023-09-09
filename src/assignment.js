7f1479ff860b9b667504f49c4e134ee8994738a7
SukunimiVinod <Vinodvinod19032022@gmail.com>
2023-09-09 13:10:46 +0300

    first exercise finally odne

--- src/assignment.js
+++ src/assignment.js
@@ -3,12 +3,10 @@ let firstNumber = 10
 firstNumber = 0
 
 // TODO: 1. Set the value of firstNumber below so the tests pass
-firstNumber = 5
+firstNumber = 20
 console.log(firstNumber)
 // TODO: 2. Change the code below so that the tests pass
-let secondNumber = 0 // edit this value
-secondNumber = 8
-console.log(secondNumber)
+const secondNumber = 42 // edit this value
 // do not edit the exported object.
 module.exports = {
   a: firstNumber,
--- src/declaration.js
+++ src/declaration.js
@@ -2,8 +2,8 @@
 //
 //
 // TODO: 1. Declare the variables firstName and age so that the tests pass
-const firstName = 'Bob'
-const age = 26
+const firstName = 'Jane'
+const age = 35
 console.log(firstName, age)
 // do not edit below this line
 let firstNameExport = ''
